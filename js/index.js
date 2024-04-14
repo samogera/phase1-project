5b7f1507d84004e86c540bec667fbf5401951e27
samogera <samuelohillary@gmail.com>
2024-04-14 22:48:46 +0300

    made more changes to javascript

--- js/index.js
+++ js/index.js
@@ -1,52 +1,64 @@
 window.onload = function() {
     // Fetch cat history from Wikipedia API
+    fetchCatHistory();
+
+    // Fetch random cat fact from Cat Facts API
+    fetchCatFact();
+
+    // Load cat images from db.json
+    fetchCatImages();
+};
+
+function fetchCatHistory() {
     fetch('https://en.wikipedia.org/api/rest_v1/page/summary/Cat')
         .then(response => response.json())
         .then(data => {
             const historySection = document.getElementById('history');
             historySection.innerHTML = `
-          <h2>Cat History</h2>
-          <p>${data.extract}</p>
-        `;
+                <h2>Cat History</h2>
+                <p>${data.extract}</p>
+            `;
         })
         .catch(error => {
             console.error('Error fetching cat history:', error);
         });
+}
 
-    // Fetch random cat fact from Cat Facts API
+function fetchCatFact() {
     fetch('https://catfact.ninja/fact')
         .then(response => response.json())
         .then(data => {
             const factsSection = document.getElementById('facts');
             factsSection.innerHTML = `
-          <h2>Random Cat Fact</h2>
-          <p>${data.fact}</p>
-        `;
+                <h2>Random Cat Fact</h2>
+                <p>${data.fact}</p>
+            `;
         })
         .catch(error => {
             console.error('Error fetching cat fact:', error);
         });
+}
 
-    // Load cat images from db.json
+function fetchCatImages() {
     fetch('db.json')
-        .then(response => {
-            console.log('Response:', response);
-            return response.json();
-        })
+        .then(response => response.json())
         .then(data => {
-            console.log('Data:', data);
             const catImages = data.cats;
             const randomIndex = Math.floor(Math.random() * catImages.length);
             const randomCatImageUrl = catImages[randomIndex].url;
-            const catCanvas = document.getElementById('catCanvas');
-            const ctx = catCanvas.getContext('2d');
-            const catImage = new Image();
-            catImage.onload = function() {
-                ctx.drawImage(catImage, 0, 0, catCanvas.width, catCanvas.height);
-            };
-            catImage.src = randomCatImageUrl;
+            displayCatImage(randomCatImageUrl);
         })
         .catch(error => {
             console.error('Error fetching cat images:', error);
         });
-};
+}
+
+function displayCatImage(imageUrl) {
+    const catCanvas = document.getElementById('catCanvas');
+    const ctx = catCanvas.getContext('2d');
+    const catImage = new Image();
+    catImage.onload = function() {
+        ctx.drawImage(catImage, 0, 0, catCanvas.width, catCanvas.height);
+    };
+    catImage.src = imageUrl;
+}
